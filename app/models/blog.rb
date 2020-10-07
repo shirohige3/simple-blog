24b64b9f581139e3eebce4504d91b4d3102aa702
shirohige3 <shirohigesan08@gmail.com>
2020-10-07 18:03:48 +0900

    バリデーション設定(user.blog.comment)

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,11 @@
 class Blog < ApplicationRecord
   has_many   :comments
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+  validates :title
+  validates :text
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
          has_many :blogs
          has_many :comments
+         has_one_attached :image
+
+         validates :nickname,       presence: true
+
+         validates :full_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :full_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+         validates :email,          presence: true, format: { with: /@+/ }
+         
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i.freeze
+         validates :password,       presence: true, format: { with: PASSWORD_REGEX }
+
 end
