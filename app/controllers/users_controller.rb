372f0e2a30b06a46c32224c2a8fa7309a80051ee
shirohige3 <shirohigesan08@gmail.com>
2020-10-09 11:52:08 +0900

    ログイン・ログアウト機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
     end
   end
 
+
+
     private
     def user_params
       params.require(:user).permit(:nickname, :email, :password, :full_name, :full_name_kana, :birth_date, :introduction)
