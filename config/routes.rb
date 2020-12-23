d5175ca601516552363c68334eb98ce6ee13588a
shirohige3 <shirohigesan08@gmail.com>
2020-12-23 10:41:51 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,9 @@ Rails.application.routes.draw do
      get "search"
     end
   end  
+
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
